0f61088c370d3917db5023e2ff5a88491841fd59
Amar Mule <amarmulay23@gmail.com>
2023-03-06 14:37:41 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+
+  // 
+  const [data, setData] = useState([])
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products').then((result) => {
+      result.json().then((responce) => {
+        // console.warn("result", responce)
+        setData(responce)
+      })
+    })
+  }, [])
+  console.warn(data)
+  // 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>my-api</h1>
+ {/*  */}
+ {/* how to create table using api */}
+          <table border={1}>
+            <tbody>
+            <tr>
+              <th>id</th>
+              <th>title</th>
+              <th>price</th>
+              <th>description</th>
+            </tr>
+
+          
+                  {
+                    data.map((item)=>
+                        <tr>
+                          <td>{item.id}</td>
+                          <td>{item.title}</td>
+                          <td>{item.price}</td>
+                          <td>{item.description}</td>
+                        </tr>
+                    )
+                  }
+              </tbody>
+          </table>
+  {/*  */}
+<hr/>
+{/*  */}
+{/* hoe toget data from upi */}
+                  {
+                    data.map((item)=>
+                    <div>
+                    <p>{item.id}</p>
+                    <p>{item.title}</p>
+                    <p>{item.price}</p>
+                    <p>{item.description}</p>
+                    </div>
+                    )
+                  }
+ {/*  */}
+
     </div>
   );
 }
